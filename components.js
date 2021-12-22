aa56c0f005d59ddec6945d8196998d346aed4c78
Saelens Aurelien <aureliensaelens0@gmail.com>
2021-12-22 16:35:29 +0100

    Ajout des composants

---
+++ components.js
@@ -0,0 +1,34 @@
+// CHARGEMENT DES IMAGES //
+
+const BG_IMG = new Image();
+BG_IMG.src = "images/ciel.jpeg";
+
+const LEVEL_IMG = new Image();
+LEVEL_IMG.src = "images/level.png";
+
+const LIFE_IMG = new Image();
+LIFE_IMG.src = "images/life.png";
+
+const SCORE_IMG = new Image();
+SCORE_IMG.src = "images/score.png";
+// FIN DE CHARGEMENT DES IMAGES // 
+
+// ******************** // 
+
+// CHARGEMENT DES SONS //
+
+const WALL_HIT = new Audio();
+WALL_HIT.src = "sons/wall.mp3";
+
+const LIFE_LOST = new Audio();
+LIFE_LOST.src = "sons/life_lost.mp3";
+
+const PADDLE_HIT = new Audio();
+PADDLE_HIT.src = "sons/paddle_hit.mp3";
+
+const WIN = new Audio();
+WIN.src = "sons/win.mp3";
+
+const BRICK_HIT = new Audio();
+BRICK_HIT.src = "sons/brick_hit.mp3";
+// FIN DE CHARGEMENT DES SONS //
